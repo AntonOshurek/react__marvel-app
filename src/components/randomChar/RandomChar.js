0ef589f1dd47eba6df94eaf156d37bf88b9616b1
AntonOshurek <oshurekweb@gmail.com>
2021-12-08 23:08:04 +0100

    add spiner to random character

--- src/components/randomChar/RandomChar.js
+++ src/components/randomChar/RandomChar.js
@@ -1,6 +1,7 @@
 import { Component } from 'react';
 
-import MarvelService from '../../services/MarvelService'
+import MarvelService from '../../services/MarvelService';
+import Spinner from '../spiner/spiner';
 
 import './randomChar.scss';
 
@@ -12,13 +13,14 @@ class RandomChar extends Component {
   }
 
   state = {
-    character: {}
+    character: {},
+    loading: true
   }
 
   marvelService = new MarvelService();
 
   onCharacterLoaded = (character) => {
-    this.setState({character});
+    this.setState({character, loading: false});
   }
 
   updateChar = () => {
@@ -33,28 +35,14 @@ class RandomChar extends Component {
   }
 
   render() {
-    const {character: {name, description, thumbnails, homepage, wiki}} = this.state;
+    const {character, loading} = this.state;
 
     return (
       <section className="randomchar">
         <h2 className="visually-hidden">random character for you!</h2>
-        <article className="randomchar__block">
-          <img className="randomchar__img" width="180" height="180" src={thumbnails} alt={'Random character - ' + name}/>
-          <div className="randomchar__info">
-            <p className="randomchar__name">{name}</p>
-            <p className="randomchar__descr">
-              {description ? description : 'sory! We dont have description for this character'}
-            </p>
-            <div className="randomchar__links">
-              <a className="button button__main" href={homepage}>
-                <div className="inner">homepage</div>
-              </a>
-              <a className="button button__secondary" href={wiki}>
-                <div className="inner">Wiki</div>
-              </a>
-            </div>
-          </div>
-        </article>
+
+        {loading ? <Spinner/> : <View character={character}/>}
+
         <article className="randomchar__static">
           <p className="randomchar__title">
             Random character for today!<br/>
@@ -71,6 +59,30 @@ class RandomChar extends Component {
     )
   }
 
+}
+
+const View = ({character}) => {
+  const {name, description, thumbnails, homepage, wiki} = character;
+
+  return (
+    <article className="randomchar__block">
+      <img className="randomchar__img" width="180" height="180" src={thumbnails} alt={'Random character - ' + name}/>
+      <div className="randomchar__info">
+        <p className="randomchar__name">{name}</p>
+        <p className="randomchar__descr">
+          {description ? description : 'sory! We dont have description for this character'}
+        </p>
+        <div className="randomchar__links">
+          <a className="button button__main" href={homepage}>
+            <div className="inner">homepage</div>
+          </a>
+          <a className="button button__secondary" href={wiki}>
+            <div className="inner">Wiki</div>
+          </a>
+        </div>
+      </div>
+    </article>
+  )
 }
 
 export default RandomChar;
--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -8,7 +8,6 @@ class MarvelService {
 
     if(!res.ok) {
       throw new Error(`Could not fetch ${url}, status: ${res.status}`);
-
     }
 
     return await res.json();
