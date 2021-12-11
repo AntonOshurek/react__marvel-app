41c32e7b60439cd04161af422add45e0c03a675a
AntonOshurek <oshurekweb@gmail.com>
2021-12-11 23:55:50 +0100

    add characters

--- src/components/charList/CharList.js
+++ src/components/charList/CharList.js
@@ -1,53 +1,85 @@
-import './charList.scss';
-import abyss from '../../resources/img/abyss.jpg';
+import { Component } from 'react/cjs/react.production.min';
 
-const CharList = () => {
-  return (
-    <section className="characters">
-      <h2 className="visually-hidden">Characters list!</h2>
-      <ul className="characters__list">
-        <li className="characters__item">
-          <img className="characters__image" width="200" height="200" src={abyss} alt="abyss"/>
-          <h3 className="characters__name">Abyss</h3>
+import Spinner from '../spiner/spiner';
+import ErrorMessage from '../errorMessage/ErrorMessage';
+import MarvelService from '../../services/MarvelService';
+
+import './charList.scss';
+//import abyss from '../../resources/img/abyss.jpg';
+
+class CharList extends Component {
+
+  state = {
+    charList: [],
+    loading: true,
+    error: false
+  }
+
+  marvelService = new MarvelService();
+
+  componentDidMount() {
+    this.marvelService.getAllCharacters()
+      .then(this.onCharListLoaded)
+      .catch(this.onError)
+  }
+
+  onCharListLoaded = (charList) => {
+    this.setState({
+      charList,
+      loading: false
+    })
+  }
+
+  onError = () => {
+    this.setState({
+      error: true,
+      loading: false
+    })
+  }
+
+  renderItems(arr) {
+    const items =  arr.map((item) => {
+      let imgStyle = {'objectFit' : 'cover'};
+      if (item.thumbnail === 'http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg') {
+        imgStyle = {'objectFit' : 'unset'};
+      }
+
+      return (
+        <li className="characters__item" key={item.id}>
+          <img className="characters__image" width="200" height="200" src={item.thumbnail} alt={item.name} style={imgStyle}/>
+          <h3 className="characters__name">{item.name}</h3>
         </li>
-        <li className="characters__item characters__item--selected">
-          <img className="characters__image" width="200" height="200" src={abyss} alt="abyss"/>
-          <h3 className="characters__name">Abyss</h3>
-        </li>
-        <li className="characters__item">
-          <img className="characters__image" width="200" height="200" src={abyss} alt="abyss"/>
-          <h3 className="characters__name">Abyss</h3>
-        </li>
-        <li className="characters__item">
-          <img className="characters__image" width="200" height="200" src={abyss} alt="abyss"/>
-          <h3 className="characters__name">Abyss</h3>
-        </li>
-        <li className="characters__item">
-          <img className="characters__image" width="200" height="200" src={abyss} alt="abyss"/>
-          <h3 className="characters__name">Abyss</h3>
-        </li>
-        <li className="characters__item">
-          <img className="characters__image" width="200" height="200" src={abyss} alt="abyss"/>
-          <h3 className="characters__name">Abyss</h3>
-        </li>
-        <li className="characters__item">
-          <img className="characters__image" width="200" height="200" src={abyss} alt="abyss"/>
-          <h3 className="characters__name">Abyss</h3>
-        </li>
-        <li className="characters__item">
-          <img className="characters__image" width="200" height="200" src={abyss} alt="abyss"/>
-          <h3 className="characters__name">Abyss</h3>
-        </li>
-        <li className="characters__item">
-          <img className="characters__image" width="200" height="200" src={abyss} alt="abyss"/>
-          <h3 className="characters__name">Abyss</h3>
-        </li>
-      </ul>
-      <button className="characters__load-button button button__main" type="button">
-        <div className="inner">load more</div>
-      </button>
-    </section>
-  )
+      )
+    });
+    return (
+        <ul className="characters__list">
+            {items}
+        </ul>
+    )
+}
+
+  render() {
+
+    const {charList, loading, error} = this.state;
+
+    const items = this.renderItems(charList);
+
+    const errorMessage = error ? <ErrorMessage/> : null;
+    const spinner = loading ? <Spinner/> : null;
+    const content = !(loading || error) ? items : null;
+
+    return (
+      <section className="characters">
+        <h2 className="visually-hidden">Characters list!</h2>
+        {errorMessage}
+        {spinner}
+        {content}
+        <button className="characters__load-button button button__main" type="button">
+          <div className="inner">load more</div>
+        </button>
+      </section>
+    )
+  }
 }
 
 export default CharList;
