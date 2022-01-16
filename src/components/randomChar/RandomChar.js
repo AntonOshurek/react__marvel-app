53d61d7fb9a975c90a259580381fdddfae853617
AntonOshurek <oshurekweb@gmail.com>
2022-01-16 23:09:45 +0100

    random character to hooks

--- src/components/randomChar/RandomChar.js
+++ src/components/randomChar/RandomChar.js
@@ -1,4 +1,4 @@
-import { Component } from 'react';
+import { useState, useEffect } from 'react';
 
 import MarvelService from '../../services/MarvelService';
 import Spinner from '../spiner/spiner';
@@ -6,89 +6,83 @@ import ErrorMessage from '../errorMessage/ErrorMessage';
 
 import './randomChar.scss';
 
-class RandomChar extends Component {
+const RandomChar = () => {
 
-  state = {
-    character: {},
-    loading: true,
-    error: false
+  const [character, setCharacter] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(false);
+
+  const marvelService = new MarvelService();
+
+  useEffect(() => {
+    updateChar();
+    const timerId = setInterval(updateChar, 60000);
+
+    return () => {
+      clearInterval(timerId)
+    }
+  }, []);
+
+  const onCharacterLoaded = (character) => {
+    setLoading(false);
+    setCharacter(character);
   }
 
-  marvelService = new MarvelService();
-
-  componentDidMount() {
-    this.updateChar();
-    //this.timerId = setInterval(this.updateChar, 3000);
+  const onCharLoading = () => {
+    setLoading(true)
   }
 
-  // componentWillUnmount() {
-  //   clearInterval(this.timerId);
-  // }
-
-  onCharacterLoaded = (character) => {
-    this.setState({character, loading: false});
+  const onError = () => {
+    setError(true);
+    setLoading(false);
   }
 
-  onCharLoading = () => {
-    this.setState({
-      loading: true
-    })
-}
-
-  onError = () => {
-    this.setState({loading: false, error: true});
-  }
-
-  updateChar = () => {
+  const updateChar = () => {
     const minValue = 1011000;
     const maxValue = 1011400;
 
     const id = Math.floor(Math.random() * (maxValue - minValue) + minValue);
-    this.onCharLoading();
-    this.marvelService
+    onCharLoading();
+    marvelService
       .getCharacter(id)
-        .then(this.onCharacterLoaded)
-        .catch(this.onError)
+        .then(onCharacterLoaded)
+        .catch(onError)
   }
 
-  render() {
-    const {character, loading, error} = this.state;
-    const errorMessage = error ? <ErrorMessage/> : null;
-    const spiner = loading ? <Spinner/> : null;
-    const content = !(loading || error) ? <View character={character}/> : null;
+  const errorMessage = error ? <ErrorMessage/> : null;
+  const spiner = loading ? <Spinner/> : null;
+  const content = !(loading || error || !character) ? <View character={character}/> : null;
 
-    return (
-      <section className="randomchar">
-        <h2 className="visually-hidden">random character for you!</h2>
+  return (
+    <section className="randomchar">
+      <h2 className="visually-hidden">random character for you!</h2>
 
-        {errorMessage}
-        {spiner}
-        {content}
+      {errorMessage}
+      {spiner}
+      {content}
 
-        <article className="randomchar__static">
-          <p className="randomchar__title">
-            Random character for today!<br/>
-            Do you want to get to know him better?
-          </p>
-          <p className="randomchar__title">
-            Or choose another one
-          </p>
-          <button onClick={this.updateChar} className="randomchar__try-button button button__main" type="button">
-            <div className="inner">try it</div>
-          </button>
-        </article>
-      </section>
-    )
-  }
+      <article className="randomchar__static">
+        <p className="randomchar__title">
+          Random character for today!<br/>
+          Do you want to get to know him better?
+        </p>
+        <p className="randomchar__title">
+          Or choose another one
+        </p>
+        <button onClick={updateChar} className="randomchar__try-button button button__main" type="button">
+          <div className="inner">try it</div>
+        </button>
+      </article>
+    </section>
+  )
 
 }
 
 const View = ({character}) => {
   const {name, description, thumbnails, homepage, wiki} = character;
-
   let imgStyle = {'objectFit' : 'cover'};
   if (thumbnails === 'http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg') {
-      imgStyle = {'objectFit' : 'contain'};
+    imgStyle = {'objectFit' : 'contain'};
   }
 
   return (
