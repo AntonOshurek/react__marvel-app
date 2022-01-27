0bbefa977160312657b871fbe5ce51b3fd057fbf
AntonOshurek <oshurekweb@gmail.com>
2022-01-27 19:02:15 +0100

    add banner to comics list

--- src/components/pages/ComicsPage.js
+++ src/components/pages/ComicsPage.js
@@ -1,10 +1,12 @@
 import ComicsList from "../comicsList/ComicsList";
+import AppBanner from "../appBanner/AppBanner";
 
 const ComicsPage = () => {
 
 
   return (
     <>
+      <AppBanner/>
       <ComicsList/>
     </>
   )
