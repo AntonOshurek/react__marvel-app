6d4272545cda24ed6b0364552a989ff40b33b4ac
AntonOshurek <oshurekweb@gmail.com>
2022-01-29 13:19:16 +0100

    add banner adaptive styles

--- src/components/appBanner/AppBanner.js
+++ src/components/appBanner/AppBanner.js
@@ -3,16 +3,20 @@ import avengers from '../../resources/img/Avengers.png';
 import avengersLogo from '../../resources/img/Avengers_logo.png';
 
 const AppBanner = () => {
-    return (
-        <div className="app__banner">
-            <img src={avengers} alt="Avengers"/>
-            <div className="app__banner-text">
-                New comics every week!<br/>
-                Stay tuned!
-            </div>
-            <img src={avengersLogo} alt="Avengers logo"/>
-        </div>
-    )
+  return (
+    <div className="banner">
+      <img className='banner__img'
+        width={'100px'} height={'120px'}
+        src={avengers} alt="Avengers"/>
+      <div className="banner__text">
+          New comics every week!<br/>
+          Stay tuned!
+      </div>
+      <img className='banner__img'
+        width={'100px'} height={'120px'}
+        src={avengersLogo} alt="Avengers logo"/>
+    </div>
+  )
 }
 
 export default AppBanner;
