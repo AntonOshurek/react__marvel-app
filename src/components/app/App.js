c74c475ba4c214371dbd3ac1cf2b2f6780f947cb
AntonOshurek <oshurekweb@gmail.com>
2022-02-17 23:05:28 +0100

    add new single charater page and new routing

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -7,7 +7,10 @@ import Spinner from "../spiner/spiner";
 const Page404 = lazy(() => import("../pages/404"));
 const MainPage = lazy(() => import("../pages/MainPage"));
 const ComicsList = lazy(() => import("../comicsList/ComicsList"));
-const SingleComicPage = lazy(() => import("../pages/SingleComicPage"));
+// const SingleComicPage = lazy(() => import("../pages/SingleComicPage"));
+const SingleComicPage = lazy(() => import('../pages/SingleComicPage'));
+const SingleCharacterPage = lazy(() => import('../pages/SingleCharacterPage'));
+const SinglePage = lazy(() => import('../pages/SinglePage'));
 
 const App = () => {
 
@@ -20,7 +23,14 @@ const App = () => {
               <Routes>
                 <Route path="/react__marvel-app" element={<MainPage/>}/>
                 <Route path="/comics" element={<ComicsList/>}/>
-                <Route path="/comics/:comicId" element={<SingleComicPage/>}/>
+                {/* <Route path="/comics/:Id" Component={SingleComicPage} dataType='comic' /> */}
+                {/* element={<SingleComicPage/>} */}
+                <Route exact path="/comics/:id" element={<SinglePage Component={SingleComicPage} dataType='comic'/>}/>
+                    {/* <SinglePage Component={SingleComicPage} dataType='comic'/> */}
+                {/* </Route> */}
+                <Route exact path="/characters/:id" element={<SinglePage Component={SingleCharacterPage} dataType='character'/>}/>
+                    {/* <SinglePage Component={SingleCharacterPage} dataType='character'/> */}
+                {/* </Route> */}
                 <Route path="*" element={<Page404/>}/>
               </Routes>
             </Suspense>
--- src/components/pages/index.js
+++ src/components/pages/index.js
@@ -1,6 +1,7 @@
 import MainPage from "../pages/MainPage";
-import ComicsPage from "../pages/ComicsPage";
+// import ComicsPage from "../pages/ComicsPage";
 import Page404 from "./404";
 import SingleComicPage from "./SingleComicPage";
 
-export {MainPage, ComicsPage, Page404, SingleComicPage};
+export {MainPage, Page404, SingleComicPage};
+// ComicsPage
